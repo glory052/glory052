c3aaace632674527fbdb8220f11573895d8083ff
glory052 <sakraglory@gmail.com>
2025-05-03 13:37:50 +0530

    Add files via upload

---
+++ common values.py
@@ -0,0 +1,8 @@
+array1=[1,2,3,4,5]
+array2=[4,5,6,7,8]
+
+common_values=set(array1).intersection(set(array2))
+
+common_values=list(common_values)
+
+print("common values between the two arrays:",common_values)
